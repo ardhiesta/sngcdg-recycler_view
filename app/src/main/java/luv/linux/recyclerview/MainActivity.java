5dae65e4203c9d71cabdb13db00f88ec26ab2dbf
ardhiesta <ardhi@wonderlabs.io>
2017-01-05 13:19:29 +0700

    initial import

---
+++ app/src/main/java/luv/linux/recyclerview/MainActivity.java
@@ -0,0 +1,40 @@
+package luv.linux.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import luv.linux.recyclerview.model.Person;
+import luv.linux.recyclerview.utils.RVAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    private List<Person> persons;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initializeData();
+
+        RecyclerView rv = (RecyclerView) findViewById(R.id.rv);
+//        rv.setHasFixedSize(true);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+
+        RVAdapter adapter = new RVAdapter(persons);
+        rv.setAdapter(adapter);
+    }
+
+    private void initializeData(){
+        persons = new ArrayList<>();
+        persons.add(new Person("Emma Wilson", "23 years old", R.drawable.ubuntu));
+        persons.add(new Person("Lavery Maiss", "25 years old", R.drawable.lm));
+        persons.add(new Person("Lillie Watts", "35 years old", R.drawable.debian));
+    }
+}
---
+++ app/src/main/java/luv/linux/recyclerview/utils/RVAdapter.java
@@ -0,0 +1,65 @@
+package luv.linux.recyclerview.utils;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import luv.linux.recyclerview.R;
+import luv.linux.recyclerview.model.Person;
+
+/**
+ * Created by linuxluv on 1/5/17.
+ */
+
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.PersonViewHolder> {
+    List<Person> persons;
+
+    public RVAdapter(List<Person> persons){
+        this.persons = persons;
+    }
+
+    @Override
+    public RVAdapter.PersonViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_card, parent, false);
+        PersonViewHolder pvh = new PersonViewHolder(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(RVAdapter.PersonViewHolder personViewHolder, int position) {
+        personViewHolder.personName.setText(persons.get(position).getName());
+        personViewHolder.personAge.setText(persons.get(position).getAge());
+        personViewHolder.personPhoto.setImageResource(persons.get(position).getPhotoId());
+    }
+
+    @Override
+    public int getItemCount() {
+        return persons.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    public static class PersonViewHolder extends RecyclerView.ViewHolder {
+        CardView cv;
+        TextView personName;
+        TextView personAge;
+        ImageView personPhoto;
+
+        PersonViewHolder(View itemView) {
+            super(itemView);
+            cv = (CardView)itemView.findViewById(R.id.cv);
+            personName = (TextView)itemView.findViewById(R.id.person_name);
+            personAge = (TextView)itemView.findViewById(R.id.person_age);
+            personPhoto = (ImageView)itemView.findViewById(R.id.person_photo);
+        }
+    }
+}
